f93e73a19c5b1e9b8d1c1e20bd9ebe616f25894d
werido <359066432@qq.com>
2022-08-23 00:47:37 +0800

    📃 docs: update redis

--- docs/redis/start.sh
+++ docs/redis/start.sh
@@ -5,6 +5,6 @@ redis-server cluster/7002/redis.conf
 redis-server cluster/7003/redis.conf
 redis-server cluster/7004/redis.conf
 redis-server cluster/7005/redis.conf
-
+redis-server cluster/7006/redis.conf
 
 
--- docs/redis/stop.sh
+++ docs/redis/stop.sh
@@ -4,3 +4,4 @@ redis-cli -c -h 127.0.0.1 -p 7002 shutdown
 redis-cli -c -h 127.0.0.1 -p 7003 shutdown
 redis-cli -c -h 127.0.0.1 -p 7004 shutdown
 redis-cli -c -h 127.0.0.1 -p 7005 shutdown
+redis-cli -c -h 127.0.0.1 -p 7006 shutdown

9f51aaaeb497c8f76fd27604e4d758e551fd5ae4
werido <359066432@qq.com>
2022-10-18 16:55:54 +0800

    📃 docs: update

--- docs/pool/_threadPool.py
+++ docs/pool/_threadPool.py
@@ -2,21 +2,31 @@ from concurrent.futures import ThreadPoolExecutor,as_completed, thread
 import threading
 import time,datetime
 
+event  = threading.Event()
+
 def task(seconds):
-    time.sleep(10)
+    print(">>>",seconds)
+    # event = threading.Event()
+    event.wait(1000)
+
+
+def wakeup():
+    event.set()
 
 
 if __name__ =="__main__":
-    pool = ThreadPoolExecutor()
+    pool = ThreadPoolExecutor(max_workers=2)
     futs = []
-    for i in range(0,10,2):
-        fut = pool.submit(task,i)
-        futs.append(fut)
+    # for i in range(0,10,2):
+    #     fut = pool.submit(task,i)
+    #     futs.append(fut)
+    pool.submit(task,22)
+    time.sleep(2)
+    pool.submit(wakeup)
     
-    
-    res = as_completed(futs,timeout=5)
-    for r in res:
-        print(datetime.datetime.now())
-        print(r)
-        print(datetime.datetime.now())
+    # res = as_completed(futs)
+    # for r in res:
+    #     print(datetime.datetime.now())
+    #     print(r)
+    #     print(datetime.datetime.now())
   
--- test.py
+++ test.py
@@ -1,6 +1,2 @@
-import sys
-if sys.platform == "win32":
-    from _ssl import enum_certificates, enum_crls
-    
-
-print(enum_certificates("ROOT"))
+from certifi import where
+print(where())
